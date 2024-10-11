9305f6411add9da5b3700ecb181ad46224ea3d4f
VarshithNaiduLPU <varshithnaidu@lpu.in>
2024-10-12 05:02:39 +0530

    Landing Page

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -14,6 +14,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+        <head>
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
+            <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400..900&display=swap" rel="stylesheet" />
+        </head>
       <body>
         {children}
       </body>
