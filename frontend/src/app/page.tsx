2bea1038358b710121df03c3bade551f47d0aec5
VarshithNaiduLPU <varshithnaidu@lpu.in>
2024-10-13 00:11:36 +0530

    Added Github Auth

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -12,16 +12,19 @@ export default function Home() {
         .then((data) => {
             setData(data);
         })
-        .finally(() => {
-            console.log(apiData);
-        })
-
     }, [])
+
+    function HandleClick() {
+        window.location.replace(`https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_CLIENT_ID}&scope=read:user repo`);
+    }
+
   return (
     <>
         <section className="main">
             <h1>Project-Z</h1>
             <p>Coming Soon</p>
+            <p>{apiData}</p>
+            <button onClick={HandleClick} className="btn">Signin</button>
         </section>
     </>
   );
